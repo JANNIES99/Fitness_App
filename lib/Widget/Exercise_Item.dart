f7719a10961c6045c29dae51cc273ee8f82c9aa8
JANNIES <janniesvarghese@gmail.com>
2024-12-29 21:08:38 +0530

    first

---
+++ lib/Widget/Exercise_Item.dart
@@ -0,0 +1,16 @@
+import 'package:fitnessapp/model/exercise.dart';
+import 'package:flutter/material.dart';
+
+class ExerciseItem extends StatelessWidget {
+  const ExerciseItem({required this.exercise, super.key});
+  final Exercise exercise;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
