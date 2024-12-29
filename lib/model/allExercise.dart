365a1b3b8ec7756fef8940dcd11db3f9da7aada8
JANNIES <janniesvarghese@gmail.com>
2024-12-29 21:03:58 +0530

    first

---
+++ lib/model/allExercise.dart
@@ -0,0 +1,10 @@
+import 'package:fitnessapp/model/Exercise.dart';
+
+List<Exercise> absEasyExercise = [
+  Exercise.withRepetition(
+      name: "Abdominal Crunches",
+      focusArea: [AreasOfTheBody.abs],
+      instructions:
+          "Lie on your back with your knees bent and feet flat on the floor, hip-width apart. Cross your arms over your chest with your palms just below your shoulders. Engage your core by drawing your belly button and waist towards your spine. Tuck your chin in slightly, as if holding a tennis ball between your chin and chest.\n Slowly tense your upper abdominals to lift your head, then neck, then shoulders and upper back off the floor as you reach forwards and up with your hands. Keep your lower back on the floor and avoid arching it away from the floor. From the top position, slowly lower your upper back, then shoulders, then neck and head back to the starting position.",
+      repetition: 16)
+];
