88c957a36206dbc86e384fdf611a2519323c9e58
JANNIES <janniesvarghese@gmail.com>
2024-12-30 23:26:32 +0530

    first

--- lib/View/Exercise_List_View.dart
+++ lib/View/Exercise_List_View.dart
@@ -1,12 +1,21 @@
+import 'package:fitnessapp/model/Exercise.dart';
 import 'package:flutter/material.dart';
 
 class ExerciseListView extends StatelessWidget {
-  const ExerciseListView({super.key});
+  const ExerciseListView({required this.exercises, super.key});
+  final List<Exercise> exercises;
 
   @override
   Widget build(BuildContext context) {
+    final int length = exercises.length;
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ListView.builder(
+          itemCount: length,
+          itemBuilder: (ctx, index) {},
+        ),
+      ),
     );
   }
 }
