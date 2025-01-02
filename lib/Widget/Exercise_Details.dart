f243ea9bf6843f458a29c674b6dbfe670ca81d04
JANNIES <janniesvarghese@gmail.com>
2025-01-02 16:25:27 +0530

    first

--- lib/Widget/Exercise_Details.dart
+++ lib/Widget/Exercise_Details.dart
@@ -1,19 +1,38 @@
 import 'package:fitnessapp/model/Exercise.dart';
 import 'package:flutter/material.dart';
 
-class ExerciseDetails extends StatefulWidget {
+class ExerciseDetails extends StatelessWidget {
   const ExerciseDetails({required this.exercise, super.key});
   final Exercise exercise;
 
   @override
-  State<ExerciseDetails> createState() => _ExerciseDetailsState();
-}
-
-class _ExerciseDetailsState extends State<ExerciseDetails> {
-  @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: double.infinity,
+      child: Column(
+        children: [
+          Text(exercise.name),
+          if (exercise.isRepetition)
+            Row(
+              children: [
+                const Text("REPEATS"),
+                const Spacer(),
+                Text("x${exercise.repetition}"),
+              ],
+            ),
+          if (exercise.isTimer)
+            Row(
+              children: [
+                const Text("DURATION"),
+                const Spacer(),
+                Text("x${exercise.repetition}"),
+              ],
+            ),
+          const Text("INSTRUCTIONS"),
+          Text(exercise.instructions),
+        ],
+      ),
     );
+    ;
   }
 }
