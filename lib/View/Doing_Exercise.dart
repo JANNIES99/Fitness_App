f9a7f3191ab594fd2c8b9651efb8b7d7a7a62b14
JANNIES <janniesvarghese@gmail.com>
2025-01-04 22:30:39 +0530

    first

--- lib/View/Doing_Exercise.dart
+++ lib/View/Doing_Exercise.dart
@@ -5,6 +5,8 @@ class DoingExercise extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
