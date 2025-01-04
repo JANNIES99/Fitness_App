010dae43162b1921e7d2abcffd9fd46a8f7f971c
JANNIES <janniesvarghese@gmail.com>
2025-01-04 22:29:32 +0530

    first

---
+++ lib/View/Doing_Exercise.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DoingExercise extends StatelessWidget {
+  const DoingExercise({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
