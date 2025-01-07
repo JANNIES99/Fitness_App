ecf7172ea923ec5717a85736b41cdd8d6e8ccf8c
JANNIES <janniesvarghese@gmail.com>
2025-01-07 19:27:03 +0530

    first

---
+++ lib/View/Break_View.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class BreakView extends StatefulWidget {
+  const BreakView({super.key});
+
+  @override
+  State<BreakView> createState() => _BreakViewState();
+}
+
+class _BreakViewState extends State<BreakView> {
+  int timeForTimer = 30;
+  String timeToDisplay = "30";
+
+  void startTimer() {
+    Timer.periodic(
+      const Duration(
+        seconds: 1,
+      ),
+      (Timer t) {
+        setState(() {
+          if (timeForTimer < 1) {
+            t.cancel();
+            timeForTimer = 0;
+            timeToDisplay = timeForTimer.toString();
+          } else {
+            timeForTimer = timeForTimer - 1;
+            if (timeForTimer > 60) {
+              int t = timeForTimer;
+              int m = (t / 60).floor();
+              int s = t % 60;
+              timeToDisplay = "$m:$s";
+            } else {
+              timeToDisplay = timeForTimer.toString();
+            }
+          }
+          timeToDisplay = timeForTimer.toString();
+        });
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    startTimer();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              Text(timeToDisplay),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
