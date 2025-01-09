a4bccdf5432d8db1105e25bb8cd94f1b5da19e26
JANNIES <janniesvarghese@gmail.com>
2025-01-09 15:31:07 +0530

    first

--- lib/View/Doing_Exercise.dart
+++ lib/View/Doing_Exercise.dart
@@ -15,71 +15,69 @@ class DoingExercise extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          margin: const EdgeInsets.all(20),
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(exercise.name),
+                IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true,
+                        useSafeArea: true,
+                        builder: (BuildContext ctx) {
+                          return ExerciseDetails(exercise: exercise);
+                        },
+                      );
+                    },
+                    icon: const Icon(Icons.question_mark)),
+              ],
+            ),
+            if (exercise.isRepetition) Text("x${exercise.repetition}"),
+            if (exercise.isTimer)
+              ExerciseTimer(
+                minute: exercise.minute as int,
+                seconds: exercise.second as int,
+              ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
                 children: [
-                  Text(exercise.name),
-                  IconButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          isScrollControlled: true,
-                          useSafeArea: true,
-                          builder: (BuildContext ctx) {
-                            return ExerciseDetails(exercise: exercise);
-                          },
-                        );
-                      },
-                      icon: const Icon(Icons.question_mark)),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Row(
+                      children: [
+                        Icon(Icons.skip_previous_outlined),
+                        Text("Previous"),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Row(
+                      children: [
+                        Text("Skip"),
+                        Icon(Icons.skip_next_outlined),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-              if (exercise.isRepetition) Text("x${exercise.repetition}"),
-              if (exercise.isTimer)
-                ExerciseTimer(
-                  minute: exercise.minute as int,
-                  seconds: exercise.second as int,
-                ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Row(
-                        children: [
-                          Icon(Icons.skip_previous_outlined),
-                          Text("Previous"),
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Row(
-                        children: [
-                          Text("Skip"),
-                          Icon(Icons.skip_next_outlined),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
