00c19bcc247b8c25fde48ca727413f2e3fb54296
JANNIES <janniesvarghese@gmail.com>
2025-02-20 22:30:28 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fitnessapp/Registration/view/splashscreen.dart';
+import 'package:fitnessapp/firebase_options.dart';
 import 'package:fitnessapp/model/Theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions().currentPlatform);
+  WidgetsFlutterBinding.ensureInitialized();
+  //SharedPreferences prefs = await SharedPreferences.getInstance();
+  //bool hasSeenOnboarding = prefs.getBool('seenOnboarding') ?? false;
+
   runApp(const MyApp());
 }
 
