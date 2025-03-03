6caab3fb5dacdceede8d79deda058213c6e9c0d3
JANNIES <janniesvarghese@gmail.com>
2025-03-03 22:58:22 +0530

    first

---
+++ lib/model/ExercisePlain/chestExercisePlain.dart
@@ -0,0 +1,163 @@
+import 'package:fitnessapp/model/Exercise.dart';
+import 'package:fitnessapp/model/allExercise.dart';
+
+List<List<Exercise>> backExercise = [
+  [armCircle, mountainClimber, jumpingJacks, pushUp, plank],
+  [sitUp, jumpSquat, legRaise, pushUp, plank],
+  [pushUp, superman, tricepsSideStretch, squats, abdominalCrunches],
+  [pushUp, mountainClimber, reverseCrunch, calfRaise, legRaise],
+  [bridge, glutesKickback, flutterKick, armCircle, pushUp],
+  [tricepPushup, plank, jumpSquat, pushUp, singleLegCalfRaise],
+  [pushUp, superman, lunge, sitUp, jumpingJacks],
+  [reverseCrunch, armCircle, pushUp, mountainClimber, jumpingJacks, plank],
+  [jumpSquat, pushUp, sitUp, legRaise, superman, plank],
+  [pushUp, superman, tricepsSideStretch, squats, abdominalCrunches, lunge],
+  [mountainClimber, pushUp, reverseCrunch, calfRaise, legRaise, bridge],
+  [pushUp, bridge, glutesKickback, flutterKick, armCircle, jumpingJacks],
+  [
+    legRaise,
+    pushUp,
+    tricepPushup,
+    jumpSquat,
+    singleLegCalfRaise,
+    plank,
+  ],
+  [pushUp, superman, lunge, sitUp, jumpingJacks, abdominalCrunches],
+  [
+    armCircle,
+    mountainClimber,
+    jumpingJacks,
+    reverseCrunch,
+    glutesKickback,
+    pushUp,
+    plank,
+  ],
+  [sitUp, jumpSquat, legRaise, superman, pushUp, tricepsSideStretch, plank],
+  [
+    superman,
+    tricepsSideStretch,
+    squats,
+    abdominalCrunches,
+    lunge,
+    singleLegCalfRaise,
+    pushUp
+  ],
+  [
+    flutterKick,
+    pushUp,
+    mountainClimber,
+    reverseCrunch,
+    calfRaise,
+    legRaise,
+    bridge
+  ],
+  [
+    pushUp,
+    bridge,
+    glutesKickback,
+    flutterKick,
+    armCircle,
+    jumpingJacks,
+    reverseCrunch
+  ],
+  [
+    tricepPushup,
+    plank,
+    jumpSquat,
+    singleLegCalfRaise,
+    pushUp,
+    legRaise,
+    superman
+  ],
+  [pushUp, superman, lunge, sitUp, jumpingJacks, abdominalCrunches, bridge],
+  [
+    armCircle,
+    mountainClimber,
+    jumpingJacks,
+    reverseCrunch,
+    pushUp,
+    plank,
+    glutesKickback,
+    squats
+  ],
+  [
+    sitUp,
+    jumpSquat,
+    legRaise,
+    superman,
+    tricepsSideStretch,
+    flutterKick,
+    pushUp,
+    plank
+  ],
+  [
+    pushUp,
+    superman,
+    tricepsSideStretch,
+    squats,
+    abdominalCrunches,
+    lunge,
+    singleLegCalfRaise,
+    mountainClimber
+  ],
+  [
+    reverseCrunch,
+    calfRaise,
+    legRaise,
+    bridge,
+    flutterKick,
+    jumpingJacks,
+    pushUp,
+    mountainClimber
+  ],
+  [
+    pushUp,
+    bridge,
+    glutesKickback,
+    flutterKick,
+    armCircle,
+    jumpingJacks,
+    reverseCrunch,
+    sitUp
+  ],
+  [
+    tricepPushup,
+    plank,
+    jumpSquat,
+    singleLegCalfRaise,
+    legRaise,
+    pushUp,
+    superman,
+    lunge
+  ],
+  [
+    pushUp,
+    superman,
+    lunge,
+    sitUp,
+    jumpingJacks,
+    abdominalCrunches,
+    bridge,
+    calfRaise
+  ],
+  [
+    armCircle,
+    mountainClimber,
+    jumpingJacks,
+    reverseCrunch,
+    glutesKickback,
+    squats,
+    pushUp,
+    plank
+  ],
+  [
+    sitUp,
+    jumpSquat,
+    legRaise,
+    superman,
+    tricepsSideStretch,
+    pushUp,
+    plank,
+    flutterKick
+  ],
+];
