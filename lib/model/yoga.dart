7c9b939d1cc830ce314954c75b57690fb3a0c9ee
JANNIES <janniesvarghese@gmail.com>
2025-03-05 21:56:18 +0530

    first

---
+++ lib/model/yoga.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class Yoga {
+  Yoga({
+    required this.name,
+    required this.image,
+    required this.instructions,
+    required this.second,
+  });
+  final String name;
+  final Image image;
+  final String instructions;
+  final int second;
+}
