0dc8380cee4b00e2731233c22949434b55d1c3cc
JANNIES <janniesvarghese@gmail.com>
2025-03-08 00:23:37 +0530

    first

---
+++ lib/model/allYoga.dart
@@ -0,0 +1,35 @@
+import 'package:fitnessapp/model/yoga.dart';
+import 'package:flutter/material.dart';
+
+List<Yoga> allYoga = [
+  Yoga(
+      name: "Downward Dog",
+      image: Image.asset("images/Yoga/downwarddog.jpg"),
+      instructions:
+          "Lift your hips toward the ceiling, forming an upside-down V. \nStraighten your legs, keeping a slight bend if necessary. \nRelax your neck and let your head hang naturally. \nEngage your core muscles to support your spine. \nBreathe deeply, inhaling as you lengthen your spine and exhaling to deepen the stretch. \nHold the pose for the given time then bend your knees and return to tabletop.\nDepending on your experience and comfort level, you can hold it longer—up to 1 minute or more",
+      second: 30),
+  Yoga(
+      name: "Goddess",
+      image: Image.asset("images/Yoga/goddess.jpg"),
+      instructions:
+          "Start by standing with your feet wide apart, about 3–4 feet. \nTurn your toes out at a 45-degree angle. \nBend your knees deeply, keeping them in line with your toes. \nLower your hips so your thighs are parallel to the floor (or as low as you can comfortably go). \nKeep your chest lifted, shoulders relaxed, and arms bent at 90 degrees. \nReach your arms out parallel to the floor with palms facing forward. \nPress your feet into the mat, engaging your core and inner thighs.",
+      second: 60),
+  Yoga(
+      name: "Plank",
+      image: Image.asset("images/Yoga/plank.jpg"),
+      instructions:
+          "Start in a tabletop position with your wrists directly under your shoulders and knees under your hips. \nStep your feet back, one at a time, until your body forms a straight line from head to heels. \nEngage your core, keeping your body in a straight line (avoid letting your hips sag or rise). \nKeep your hands pressed firmly into the mat, fingers spread wide. \nEnsure your shoulders are directly over your wrists. \nLook slightly forward, keeping your neck neutral. \nHold the pose with steady breathing, keeping your body stable and engaged.",
+      second: 30),
+  Yoga(
+      name: "Tree",
+      image: Image.asset("images/Yoga/tree.jpg"),
+      instructions:
+          "Start by standing tall with your feet hip-width apart. \n Shift your weight onto one foot (e.g., left foot). \nBend the opposite knee and place the sole of that foot on the inner thigh, calf, or ankle of the standing leg (avoid the knee). \nPress your foot into the leg and your leg into the foot for stability. \nBring your hands to a prayer position at your chest, or extend them overhead with palms facing each other. \nKeep your hips squared forward and your gaze focused on a fixed point to help with balance. \nEngage your core and hold the position.",
+      second: 30),
+  Yoga(
+      name: "Warrior2",
+      image: Image.asset("images/Yoga/warrior2.jpg"),
+      instructions:
+          "Start by standing tall with your feet hip-width apart. \n Shift your weight onto one foot (e.g., left foot). \nBend the opposite knee and place the sole of that foot on the inner thigh, calf, or ankle of the standing leg (avoid the knee). \nPress your foot into the leg and your leg into the foot for stability. \nBring your hands to a prayer position at your chest, or extend them overhead with palms facing each other. \nKeep your hips squared forward and your gaze focused on a fixed point to help with balance. \nEngage your core and hold the position.",
+      second: 30),
+];
